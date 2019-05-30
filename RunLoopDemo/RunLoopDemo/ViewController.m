55e9063c18fe1ee3868353b0a21b91d982cdd0aa
ShiXianjun <1092207960@qq.com>
2019-05-30 11:15:41 +0800

    no message

--- RunLoopDemo/RunLoopDemo/ViewController.m
+++ RunLoopDemo/RunLoopDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "objc/message.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    CFRunLoopObserverRef observer = CFRunLoopObserverCreateWithHandler(CFAllocatorGetDefault(), kCFRunLoopAllActivities, YES, 0, ^(CFRunLoopObserverRef observer, CFRunLoopActivity activity) {
+        
+        switch (activity) {
+            case kCFRunLoopEntry:
+                NSLog(@"即将进入runloop");
+                break;
+            case kCFRunLoopBeforeTimers:
+                NSLog(@"即将处理 Timer");
+                break;
+            case kCFRunLoopBeforeSources:
+                NSLog(@"即将处理 Sources");
+                break;
+            case kCFRunLoopBeforeWaiting:
+                NSLog(@"即将进入休眠");
+                break;
+            case kCFRunLoopAfterWaiting:
+                NSLog(@"从休眠中唤醒loop");
+                break;
+            case kCFRunLoopExit:
+                NSLog(@"即将退出runloop");
+                break;
+                
+            default:
+                break;
+        }
+        
+    });
+    
+    CFRunLoopAddObserver(CFRunLoopGetCurrent(), observer, kCFRunLoopCommonModes);
+
+    CFRelease(observer);
 }
 
 
